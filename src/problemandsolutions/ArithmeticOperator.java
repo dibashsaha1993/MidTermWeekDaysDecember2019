c038056e5668e707470320af61e6f25b5cb11460
Dibash <dibashsaha81@gmail.com>
2020-01-16 15:50:42 -0500

    ---3----

--- src/problemandsolutions/ArithmeticOperator.java
+++ src/problemandsolutions/ArithmeticOperator.java
@@ -1,14 +1,24 @@
 package problemandsolutions;
 
+import java.util.Scanner;
+
 public class ArithmeticOperator {
     //Write Java program to allow the user to input two integer values and then the program prints the results of adding,
     // subtracting, multiplying, and dividing among the two values.
 
 
     public static void main(String[] args) {
-
+        Scanner sd = new Scanner(System.in);
+        System.out.println("enter first value");
+        double num1 = sd.nextDouble();
+        System.out.println("enter second value");
+        double num2 = sd.nextDouble();
+        System.out.println("your addition value is: " + (num1 + num2));
+        System.out.println("your subtraction value is: " + (num1 - num2));
+        System.out.println("your multiply value is: " + (num1 * num2));
+        System.out.println("your division value is: " + (num1 / num2));
     }
-
-
-
 }
+
+
+//done//
--- src/problemandsolutions/RandomNumberGenerate.java
+++ src/problemandsolutions/RandomNumberGenerate.java
@@ -1,8 +1,17 @@
 package problemandsolutions;
-
+import java.util.Random;
 public class RandomNumberGenerate {
     //Write Java program to generate a random number between 1 to 8.
-
-
-
+    public static void main(String[] args) {
+        Random rd = new Random();
+        int num;
+        for (int i = 1; i < 2; i++) {
+            num = rd.nextInt(8)+1;
+            System.out.println(num);
+        }
+    }
 }
+
+
+
+//done//
