206d3d76aa718a667c76aaefe56c2a8435a47b4b
Dibash <dibashsaha81@gmail.com>
2020-02-03 13:57:00 -0500

    __

--- src/algorithm/Numbers.java
+++ src/algorithm/Numbers.java
@@ -20,7 +20,7 @@ public class Numbers {
 
 	public static void main(String[] args) throws Exception {
 
-		int [] num = new int[1000000];
+		int [] num = new int[1000];
 		storeRandomNumbers(num);
 		ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
 
@@ -87,7 +87,7 @@ public class Numbers {
 	public static void storeRandomNumbers(int [] num){
 		Random rand = new Random();
 		for(int i=0; i<num.length; i++){
-			num[i] = rand.nextInt(1000000);
+			num[i] = rand.nextInt(1000);
 		}
 	}
 
